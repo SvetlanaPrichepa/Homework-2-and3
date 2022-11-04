74b0adfa2ff650e66ffbd9c3bef4af07a2a2074e
PrichepaSvetlana <sprichepa@yandex.by>
2022-11-04 22:01:30 +0300

    Задание 3.1

--- 3.1/Program.cs
+++ 3.1/Program.cs
@@ -7,17 +7,22 @@
 
 // 23432 -> да
 
-Console.Write("Введите число: ");
-string? number = Console.ReadLine();
+Console.WriteLine("Введите пятизначное число ");
+string number = Console.ReadLine();
+int len = number.Length;
+if (len == 5)
+{
+if (number[0] == number[4] && number[1] == number[3])
+        {
+        Console.WriteLine("Это ПАЛИДРОМ");
+        }
 
-void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
-    Console.WriteLine($"Это число: {number} - палиндром.");
-  }
-  else Console.WriteLine($"Это число: {number} - НЕ палиндром.");
+else
+    {
+        Console.WriteLine("Это не ПАЛИДРОМ.");
+    }
 }
-
-if (number!.Length == 5){
-  CheckingNumber(number);
+else
+{
+    Console.WriteLine("Учите математику. Это число не пятизначное ");
 }
-else Console.WriteLine($"Нужно пятизначное число");
